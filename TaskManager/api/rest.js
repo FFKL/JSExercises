81126aa63bb18e8375f646602aae434754a07518
dmitriy <FFKL@yandex.ru>
2016-06-30 00:40:44 +0300

    add authorization, minor changes

--- TaskManager/api/rest.js
+++ TaskManager/api/rest.js
@@ -15,6 +15,33 @@ rest.use(restify.queryParser());
 rest.use(restify.bodyParser());
 rest.use(restify.gzipResponse());
 
+
+rest.use(function(req, res, next) {
+    if (req.path().indexOf('/user') != 1) {
+        var auth = req.authorization;
+        var userId = req.url.toString().split('/')[3];
+
+        if (auth.scheme == null) {
+            res.header('WWW-Authenticate', 'Basic realm="Please login"');
+            return res.send(401);
+        }
+
+        users.findOne(auth.basic.username, function(err, user) {
+            if (user === undefined)
+                return res.send(401, {message: 'Неверный логин'});
+            if (user.password != auth.basic.password)
+                return res.send(401, {message: 'Неверный пароль'});
+            if (user.id != userId && userId != 'logout')
+                return res.send(403, {message: 'Нет доступа к ресурсу'});
+            console.log('login ' + auth.basic.username);
+            console.log('password ' + auth.basic.password);
+            return next();
+        });
+    } else {
+        return next();
+    }
+});
+
 rest.get('/api/user/:id/tasks', function (req, res) {
     tasks.list(req.params.id, function(err, tasks) {
         if (!err) {
@@ -23,7 +50,7 @@ rest.get('/api/user/:id/tasks', function (req, res) {
     })
 });
 
-rest.post('/api/user/:id/tasks/add', function (req, res) {
+rest.post('/api/user/:id/tasks', function (req, res) {
     tasks.add(req.params.id, req.params.task, function (err) {
         if (!err) {
             res.send(201, {result: 'User was added'});
@@ -47,7 +74,7 @@ rest.put('/api/user/:id/tasks/:taskId/complete', function (req, res) {
     })
 });
 
-rest.del('/api/user/:id/tasks/:taskId/delete', function (req, res) {
+rest.del('/api/user/:id/tasks/:taskId', function (req, res) {
     tasks.delete(req.params.id, req.params.taskId, function (err) {
         if (!err) {
             res.send(204, {result: 'Task was deleted'});
